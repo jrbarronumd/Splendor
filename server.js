58868499439da78bb8c9efea66b16022f327f673
jrbarronumd <jrbarronumd@gmail.com>
2021-12-27 17:11:08 -0500

    Draft html, CSS completed.

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require('express')
 const path = require('path')
 const http = require('http')
 const PORT = process.env.PORT || 8585
-const socketio = require('socket.io')
 const app = express()
 const server = http.createServer(app)
-const io = socketio(server)
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public-files")))
