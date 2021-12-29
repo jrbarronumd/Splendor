e35cffe533063b668d6f2be31cc0e0355ceab66d
jrbarronumd <jrbarronumd@gmail.com>
2021-12-29 10:27:37 -0500

    file reference correction

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 const server = http.createServer(app)
 
 // Set static folder
-app.use(express.static(path.join(__dirname, "public-files")))
+app.use(express.static(path.join(__dirname, "/public-files/")))
 
 // Start server
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
